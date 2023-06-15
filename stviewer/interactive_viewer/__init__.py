1abe9d4d516abee0e4d1770a580a2b84f2f665f0
Yao-14 <yaojiajun2021@gmail.com>
2023-06-15 14:34:33 +0800

    update

--- stviewer/interactive_viewer/__init__.py
+++ stviewer/interactive_viewer/__init__.py
@@ -1,2 +1,2 @@
-from .pv_pipeline import Viewer, init_models, add_single_model, create_plotter
+from .pv_pipeline import Viewer, add_single_model, create_plotter, init_models
 from .ui import ui_layout, ui_standard_container, ui_standard_toolbar
