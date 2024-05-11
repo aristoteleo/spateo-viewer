accb76684a369841d5fea505d6571079ec079ec6
Sichao25 <sichao_y@outlook.com>
2024-05-11 16:30:38 -0400

    delete port setting

--- stv_interactive_app.py
+++ stv_interactive_app.py
@@ -6,6 +6,5 @@ from stviewer.interactive_app import interactive_server, state
 if __name__ == "__main__":
     # upload anndata
     state.upload_anndata = None
-    opts, args = getopt.getopt(sys.argv[2:], "p", ["port="])
-    port = "1234" if len(opts) == 0 else opts[0][1]
-    interactive_server.start(port=port, timeout=0)
+
+    interactive_server.start(timeout=0)
--- stv_static_app.py
+++ stv_static_app.py
@@ -7,6 +7,4 @@ if __name__ == "__main__":
     # upload anndata
     state.selected_dir = None
 
-    opts, args = getopt.getopt(sys.argv[2:], "p", ["port="])
-    port = "1234" if len(opts) == 0 else opts[0][1]
-    static_server.start(port=port, timeout=0)
+    static_server.start(timeout=0)
